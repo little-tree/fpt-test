ee5ef7ee76c09c81d1b0b6e07341b9e4ef101730
little-tree <ngminh1988@gmail.com>
2018-01-24 14:02:04 +0700

    Initial commit

---
+++ src/main/java/fpt/minh/nguyen/FptTestApplication.java
@@ -0,0 +1,12 @@
+package fpt.minh.nguyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FptTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FptTestApplication.class, args);
+	}
+}
---
+++ src/test/java/fpt/minh/nguyen/FptTestApplicationTests.java
@@ -0,0 +1,16 @@
+package fpt.minh.nguyen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FptTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
